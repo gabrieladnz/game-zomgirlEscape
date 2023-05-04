d377dc17dc473f9fcd92c4c3113b2f06db38833c
Gabriela Diniz <100526271+gabrieladnz@users.noreply.github.com>
2023-05-04 13:19:36 -0300

    Update bird.py

--- dino_runner/components/obstacles/bird.py
+++ dino_runner/components/obstacles/bird.py
@@ -1,13 +1,15 @@
 from dino_runner.utils.constants import BIRD
 from dino_runner.components.obstacles.obstacle import Obstacle
 
-
+# inicializa o morcego
 class Bird(Obstacle):
     def __init__(self):
         super().__init__(BIRD, 0)
+        # posição inicial na tela
         self.rect.y = 180
         self.step_index = 0
-
+    
+    # desenha ele na tela
     def draw(self, screen):
         screen.blit(self.image[self.step_index // 5], self.rect)
         self.step_index += 1
