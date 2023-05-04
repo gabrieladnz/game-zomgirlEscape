98648a05f473bdec2628e79704cd05b77184bdc8
Gabriela Diniz <100526271+gabrieladnz@users.noreply.github.com>
2023-05-04 12:40:19 -0300

    Update text_utils.py

--- dino_runner/utils/text_utils.py
+++ dino_runner/utils/text_utils.py
@@ -2,11 +2,13 @@ import pygame
 
 from dino_runner.utils.constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
+# estilo do texto
 FONT_COLOR = (0, 0, 0)
 FONT_SIZE = 22
 FONT_STYLE = "freesansbold.ttf"
 
 
+# função da mensagem com seus parâmetros
 def draw_message_component(
     message,
     screen,
@@ -19,4 +21,5 @@ def draw_message_component(
     text = font.render(message, True, font_color)
     text_rect = text.get_rect()
     text_rect.center = (pos_x_center, pos_y_center)
+    # exibe a mensagem
     screen.blit(text, text_rect)
