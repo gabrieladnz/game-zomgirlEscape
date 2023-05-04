7c35337110c335e346763b5f379969503745b3e8
Gabriela Diniz <100526271+gabrieladnz@users.noreply.github.com>
2023-05-04 16:55:46 -0300

    Update obstacle.py

--- dino_runner/components/obstacles/obstacle.py
+++ dino_runner/components/obstacles/obstacle.py
@@ -9,13 +9,15 @@ class Obstacle(Sprite):
         self.type = type
         self.rect = self.image[self.type].get_rect()
         self.rect.x = SCREEN_WIDTH
-
+    
+    # atualiza opção do obstáculo na tela
     def update(self, game_speed, obstacles):
+        # direciona o objeto na direção oposta do personagem-jogador
         self.rect.x -= game_speed
 
         if self.rect.x < -self.rect.width:
             obstacles.pop()
         
-
+    # desenha o obstáculo
     def draw (self, screen):
         screen.blit(self.image[self.type], (self.rect.x, self.rect.y))
